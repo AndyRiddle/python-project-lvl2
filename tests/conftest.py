845e7a41ce857f44c0572f753d98ca617bd011bc
AndyRiddle <kas2850.cuk@yandex.ru>
2021-02-26 02:40:05 +0300

    add formater plain

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,28 @@ import pytest
 
 
 @pytest.fixture
-def changes_betwen_flat_files():
-    with open('tests/fixtures/correct_flat_diff.txt') as correct_flat_diff:
-        return correct_flat_diff.read()
+def changes_betwen_flat_files_stylish():
+    with open(
+        'tests/fixtures/correct_flat_diff_stylish.txt',
+    ) as correct_diff:
+        return correct_diff.read()
 
 
 @pytest.fixture
-def changes_betwen_files():
-    with open('tests/fixtures/correct_diff.txt') as correct_diff:
+def changes_betwen_flat_files_plain():
+    with open(
+        'tests/fixtures/correct_flat_diff_plain.txt',
+    ) as correct_diff:
+        return correct_diff.read()
+
+
+@pytest.fixture
+def changes_betwen_files_stylish():
+    with open('tests/fixtures/correct_diff_stylish.txt') as correct_diff:
+        return correct_diff.read()
+
+
+@pytest.fixture
+def changes_betwen_files_plain():
+    with open('tests/fixtures/correct_diff_plain.txt') as correct_diff:
         return correct_diff.read()
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,33 +1,65 @@
 from gendiff import generate_diff
 
 
-def test_compare_flat_json(changes_betwen_flat_files):
+def test_compare_flat_json_stylish(changes_betwen_flat_files_stylish):
     assert generate_diff(
         'tests/fixtures/flat_file_old.json',
         'tests/fixtures/flat_file_new.json',
         'stylish',
-    ) == changes_betwen_flat_files
+    ) == changes_betwen_flat_files_stylish
 
 
-def test_compare_flat_yaml(changes_betwen_flat_files):
+def test_compare_flat_yaml_stylish(changes_betwen_flat_files_stylish):
     assert generate_diff(
         'tests/fixtures/flat_file_old.yaml',
         'tests/fixtures/flat_file_new.yaml',
         'stylish',
-    ) == changes_betwen_flat_files
+    ) == changes_betwen_flat_files_stylish
 
 
-def test_compare_json(changes_betwen_files):
+def test_compare_json_stylish(changes_betwen_files_stylish):
+    assert generate_diff(
+        'tests/fixtures/file_old.json',
+        'tests/fixtures/file_new.json',
+        'stylish',
+    ) == changes_betwen_files_stylish
+
+
+def test_compare_yaml_stylish(changes_betwen_files_stylish):
     assert generate_diff(
         'tests/fixtures/file_old.yaml',
         'tests/fixtures/file_new.yaml',
         'stylish',
-    ) == changes_betwen_files
+    ) == changes_betwen_files_stylish
 
 
-def test_compare_yaml(changes_betwen_files):
+def test_compare_flat_json_plain(changes_betwen_flat_files_plain):
+    assert generate_diff(
+        'tests/fixtures/flat_file_old.json',
+        'tests/fixtures/flat_file_new.json',
+        'plain',
+    ) == changes_betwen_flat_files_plain
+
+
+def test_compare_flat_yaml_plain(changes_betwen_flat_files_plain):
+    assert generate_diff(
+        'tests/fixtures/flat_file_old.yaml',
+        'tests/fixtures/flat_file_new.yaml',
+        'plain',
+    ) == changes_betwen_flat_files_plain
+
+
+def test_compare_json_plain(changes_betwen_files_plain):
+    assert generate_diff(
+        'tests/fixtures/file_old.json',
+        'tests/fixtures/file_new.json',
+        'plain',
+    ) == changes_betwen_files_plain
+
+
+def test_compare_yaml_plain(changes_betwen_files_plain):
     assert generate_diff(
         'tests/fixtures/file_old.yaml',
         'tests/fixtures/file_new.yaml',
-        'stylish',
-    ) == changes_betwen_files
+        'plain',
+    ) == changes_betwen_files_plain
