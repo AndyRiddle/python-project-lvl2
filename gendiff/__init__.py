f60be4e3cfe790b3ae4cccca66b8fb01726409ae
AndyRiddle <kas2850.cuk@yandex.ru>
2021-03-28 22:52:40 +0300

    separation package builder

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,8 +1,8 @@
-from gendiff.builder.generate_differences import generate_diff
 # Error: Found backslash that is used for line breaking
 # Permissible to use break line
 from gendiff.formatters.format_diff import \
     STYLISH_FORMATTER as DEFAULT_FORMATTER  # noqa: N400
+from gendiff.generate_diff import generate_diff
 
 # Error: Wrong module metadata violation
 # Justified use meta-variable __all__
